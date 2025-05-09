9371bc5a5b7ef87b90ca4f377e77c97a44a62988
Abhinav Suresh <146779945+Abhi2009CU@users.noreply.github.com>
2025-05-09 17:50:11 +0000

    Create functions to calculate Star Lifetime

---
+++ src/Appv2.java
@@ -0,0 +1,63 @@
+import processing.core.PApplet;
+
+public class App extends PApplet {
+    int red,green, blue, rectx;
+    double radius, mass, lifetime;
+    public void settings(){
+        size(600,600);
+    }
+    public void setup(){
+        radius = 2;
+        red = 180;
+        green = 180;
+        blue = 180;
+        rectx = 300;
+    }
+    public void draw(){
+        background(0);
+        fill(red,green,blue);
+        ellipse(300,300, radius*100, radius*100);
+        rect(100, 505, 400, 5);
+        rect(rectx,487, 10, 40);
+        if (keyPressed){
+            if (key == CODED){
+                if (keyCode == LEFT){
+                    rectx--;
+                    if (rectx > 100){
+                        radius--;
+                    }
+                }
+                if (keyCode == RIGHT){
+                    rectx++;
+                    if (rectx < 490){
+                        radius++;
+                    }
+                }
+            }
+        }
+        if (rectx <= 100){
+            rectx = 100;
+        }
+        if (rectx >= 490){
+            rectx = 490;
+        }
+
+        // Calculations
+        mass = getMass(radius);
+        lifetime = getLifetime(mass);
+        System.out.println("For Main-Sequence Stars (like our Sun) with the radius of "+radius+" R<target_symbol>\nAn Estimated mass of: "+mass+" M<target_symbol>\nEstimated Lifetime: "+lifetime+ "years")
+
+    }
+
+    public static double getMass(double radius){
+        return Math.pow(radius, 1.25);
+    }
+
+    public static double getLifetime(double mass){
+        return 1e10/Math.pow(mass,2.5);
+    }
+
+    public static void main(String[] args){
+        PApplet.main("App");
+    }
+}
